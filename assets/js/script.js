500d8cf4434f184e48335004c82ce1cb4800f4a6
DaveyJH <daveyj.dh@googlemail.com>
2021-06-14 22:38:53 +0100

    ADD basic function structure [script.js]

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+function runGame(){
+
+}
+function checkAnswer(){
+
+}
+function calculateCorrectAnswer()){
+
+}
+function incrementScort(){
+
+}
+function incrementWrongAnswer(){
+
+}
+function displayAdditionQuestion(){
+  
+}
+function displaySubtractQuestion(){
+  
+}
+function displayMultiplyQuestion(){
+  
+}
+function displayDivideQuestion(){
+  
+}
